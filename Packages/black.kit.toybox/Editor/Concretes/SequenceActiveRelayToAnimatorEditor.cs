6e9424a1aef7cc2a7d1c4ebea824d4b1ceb0ee60
kurone-kito <krone@kit.black>
2024-02-11 13:18:57 +0900

    feat: added the sequence active relay to animation

---
+++ Packages/black.kit.toybox/Editor/Concretes/SequenceActiveRelayToAnimatorEditor.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace black.kit.toybox.Editor
+{
+    /// <summary>
+    /// The inspector of the <see cref="SequenceActiveRelayToAnimator"/>.
+    /// </summary>
+    [CustomEditor(typeof(SequenceActiveRelayToAnimator))]
+    public class SequenceActiveRelayToAnimatorEditor : EditorBase<SequenceActiveRelayToAnimator>
+    {
+        /// <summary>Initialize the editor.</summary>
+        public SequenceActiveRelayToAnimatorEditor()
+            : base(L10n.Tr(T.DETAIL_SEQUENCE_ACTIVE_RELAY_TO_ANIMATOR))
+        {
+        }
+
+        /// <summary>The callback to draw the inspector GUI.</summary>
+        public override void OnInspectorGUI()
+        {
+            DrawBanner();
+            DrawDetails();
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            var style = defaultStyle.Value;
+            EditorGUILayout.LabelField(L10n.Tr(T.USAGE_COMPONENT), style);
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField(L10n.Tr(T.USAGE_TO_ANIMATOR), style);
+            EditorGUILayout.EndVertical();
+            base.OnInspectorGUI();
+
+            serializedObject.Update();
+            AutoCompleteObject<Animator>(
+                SequenceActiveRelayToAnimator.NAME_ANIMATOR);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
