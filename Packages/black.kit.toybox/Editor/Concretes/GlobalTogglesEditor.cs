48d35488cba71e40665f052d5c0f2bd15c71f4e0
kurone-kito <krone@kit.black>
2024-04-18 07:15:46 +0900

    feat: added the function that completes the toggles

--- Packages/black.kit.toybox/Editor/Concretes/GlobalTogglesEditor.cs
+++ Packages/black.kit.toybox/Editor/Concretes/GlobalTogglesEditor.cs
@@ -1,7 +1,4 @@
-using System.Collections.Generic;
-using System.Reflection;
 using UnityEditor;
-using UnityEngine;
 using UnityEngine.UI;
 
 namespace black.kit.toybox.Editor
@@ -11,11 +8,6 @@ namespace black.kit.toybox.Editor
     public sealed class GlobalTogglesEditor
         : GlobalUIEditorBase<GlobalToggles>
     {
-        /// <summary>
-        /// The field info of the <see cref="ToggleGroup.m_Toggles"/>.
-        /// </summary>
-        private FieldInfo togglesFieldInfo;
-
         /// <summary>Initialize the editor.</summary>
         public GlobalTogglesEditor() : base(
             details: L10n.Tr(T.DETAIL_GLOBAL_TOGGLES),
@@ -28,26 +20,6 @@ namespace black.kit.toybox.Editor
         {
         }
 
-        /// <summary>
-        /// The field info of the <see cref="ToggleGroup.m_Toggles"/>.
-        /// </summary>
-        private FieldInfo ToggleFieldInfo
-        {
-            get
-            {
-                if (togglesFieldInfo == null)
-                {
-                    var flags =
-                        BindingFlags.Instance |
-                        BindingFlags.NonPublic;
-                    togglesFieldInfo = typeof(ToggleGroup).GetField(
-                        "m_Toggles",
-                        flags);
-                }
-                return togglesFieldInfo;
-            }
-        }
-
         /// <summary>The callback to draw the inspector GUI.</summary>
         public override void OnInspectorGUI()
         {
@@ -57,26 +29,11 @@ namespace black.kit.toybox.Editor
             base.OnInspectorGUI();
 
             serializedObject.Update();
-            var group = AutoCompleteObject<ToggleGroup>(
+            var toggleGroup = AutoCompleteObject<ToggleGroup>(
                 GlobalToggles.NAME_TOGGLE_GROUP);
-            if (group &&
-                ToggleFieldInfo?.GetValue(group) is List<Toggle> list)
-            {
-                var prop = serializedObject.FindProperty(
+            var arrayProp = serializedObject.FindProperty(
                 GlobalToggles.NAME_TOGGLES);
-                if (prop.arraySize != list.Count)
-                {
-                    prop.arraySize = list.Count;
-                }
-                list.Sort((a, b) => a.GetComponent<Transform>()
-                    .GetSiblingIndex().CompareTo(
-                        b.GetComponent<Transform>().GetSiblingIndex()));
-                for (var i = list.Count; --i >= 0;)
-                {
-                    prop.GetArrayElementAtIndex(i).objectReferenceValue =
-                        list[i];
-                }
-            }
+            CompleteToggles(toggleGroup, arrayProp);
             serializedObject.ApplyModifiedProperties();
         }
     }
