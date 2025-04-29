5303086a31143fbaec779102795da61d697e0344
kurone-kito <krone@kit.black>
2025-04-29 23:45:54 +0900

    feat: added the editor extension for long-press trigger component

--- Packages/black.kit.toybox/Editor/T.cs
+++ Packages/black.kit.toybox/Editor/T.cs
@@ -10,6 +10,8 @@ namespace black.kit.toybox.Editor
         public const string DETAIL_GLOBAL_SLIDER = nameof(DETAIL_GLOBAL_SLIDER);
         public const string DETAIL_GLOBAL_TOGGLE = nameof(DETAIL_GLOBAL_TOGGLE);
         public const string DETAIL_GLOBAL_TOGGLES = nameof(DETAIL_GLOBAL_TOGGLES);
+        public const string DETAIL_LONG_PRESS_TRIGGER =
+            nameof(DETAIL_LONG_PRESS_TRIGGER);
         public const string DETAIL_OBSERVER_HUB = nameof(DETAIL_OBSERVER_HUB);
         public const string DETAIL_POSITION_ANCHOR = nameof(DETAIL_POSITION_ANCHOR);
         public const string DETAIL_SEQUENCE_ACTIVE_RELAY_TO_ANIMATOR =
